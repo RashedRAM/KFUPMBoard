ea5008287ba90c1f49ab2f63d82a99f11a732463
Abdullah Alomair <s202032920@kfupm.edu.sa>
2024-05-14 13:27:53 +0300

    forcing dynamic
    added one line

--- kfupmboard/app/api/products/getUserProduct/route.js
+++ kfupmboard/app/api/products/getUserProduct/route.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
 
 export async function GET() {
     const supabase = createServerComponentClient({ cookies });
