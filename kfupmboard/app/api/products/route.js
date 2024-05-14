93f1f5ac42df9826e8579c7cc6578ae9f0d454c7
Abdullah Alomair <s202032920@kfupm.edu.sa>
2024-05-14 14:05:33 +0300

    more console logs for debugging

--- kfupmboard/app/api/products/route.js
+++ kfupmboard/app/api/products/route.js
@@ -2,19 +2,22 @@ import prisma from "@/app/libs/Prisma";
 import { NextResponse } from "next/server";
 // import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 // import { cookies } from "next/headers";
-
+    console.log("flag1")
 export async function GET() {
     // const supabase = createServerComponentClient({ cookies });
-
+    console.log("flag2")
     try {
 
+        console.log("flag3")
         const products = await prisma.Products.findMany();
 
         await prisma.$disconnect();
+        console.log("flag4")
         return NextResponse.json(products);
 
     }
     catch (error) {
+        console.log("flag5")
         console.log(error);
         await prisma.$disconnect();
         return new NextResponse('something went wrong', { status: 400 });
